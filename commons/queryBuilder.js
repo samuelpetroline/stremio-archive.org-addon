da2d07e7e2b316e15532728fc59d5d3da9082d82
Samuel Petroline <samuelpetroline@gmail.com>
2025-05-04 19:28:51 +0100

    chore: remove js files

--- commons/queryBuilder.js
+++
@@ -1,7 +0,0 @@
-const generator = require('lucene-query-generator')
-
-module.exports = (args) => {
-  return generator.convert({
-    $operands: [args],
-  })
-}
--- config/index.js
+++
@@ -1,5 +0,0 @@
-const env = require('./env')
-
-module.exports = {
-  env,
-}
